665c0b29f2f62fff2fc82a8cf2644994960f652f
marruv <marianna.vaisanen@edu.turkuamk.fi>
2018-11-22 17:19:27 +0200

    Helmet installed

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog');
 var compression = require('compression');
+var helmet = require('helmet');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(compression());
+app.use(helmet());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
